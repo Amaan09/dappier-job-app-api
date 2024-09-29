49bb179bcbcfc5fbdcd62fffc1728523eee04cbb
Mohammed Amaan <mohammedamaan79@gmail.com>
2024-09-29 18:01:40 +0530

    testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import databaseConfig from './config/database.config';
 import { UserModule } from './controllers/user/user.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(databaseConfig().uri),
-    UserModule // TEST
-  ]
+    // MongooseModule.forRoot(databaseConfig().uri),
+    // UserModule // TEST
+  ],
+  controllers: [AppController]
 })
 export class AppModule {}
