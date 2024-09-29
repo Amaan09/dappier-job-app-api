cbf41ac493868a75e740e555092107b996ebf48d
Mohammed Amaan <mohammedamaan79@gmail.com>
2024-09-29 18:43:09 +0530

    delete unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import databaseConfig from './config/database.config';
 import { UserModule } from './controllers/user/user.module';
-import { AppController } from './app.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    // MongooseModule.forRoot(databaseConfig().uri),
-    // UserModule // TEST
-  ],
-  controllers: [AppController]
+    MongooseModule.forRoot(databaseConfig().uri || 'mongodb+srv://amaan1609:GOTGJjTfqfVFr4sO@cluster0.hpo5k.mongodb.net/DappierJobApp?retryWrites=true&w=majority&appName=Cluster0'),
+    UserModule
+  ]
 })
 export class AppModule {}
