ddbb93159260d4dfd0e71b8bb032fbe40b02d52f
Mohammed Amaan <mohammedamaan79@gmail.com>
2024-10-01 02:00:34 +0530

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080, '0.0.0.0');//test 
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://dappier-job-app-ui.vercel.app'
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(8080, '0.0.0.0');
 }
 bootstrap();
