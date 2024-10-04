5fdfbb4fdd348544b0204adb1c33020bf35e3e13
Mohammed Amaan <mohammedamaan79@gmail.com>
2024-10-04 08:43:14 +0530

    add nick name in the resume creation request

--- src/domain/requests/create-resume-request.ts
+++ src/domain/requests/create-resume-request.ts
@@ -1,9 +1,9 @@
 export class CreateResumeRequest {
-    fileName: string;
+    nickName: string;
 
+    fileName: string;
 
     fileUrl: string;
 
-
-    jobDescription?: string;
+    jobDescription: string;
 }
